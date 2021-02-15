a7c1107b80d5ef095fa5153f4938d55b541b2d9c
Michael Lettrich <michael.lettrich@cern.ch>
2021-02-15 16:01:41 +0100

    [ALIGN] port AliAlgTrack

--- DataFormats/Reconstruction/include/ReconstructionDataFormats/TrackParametrization.h
+++ DataFormats/Reconstruction/include/ReconstructionDataFormats/TrackParametrization.h
@@ -139,6 +139,7 @@ class TrackParametrization
   GPUdDefault() ~TrackParametrization() = default;
 
   GPUd() void set(value_t x, value_t alpha, const params_t& par, int charge = 1, const PID pid = PID::Pion);
+  GPUd() void set(value_t x, value_t alpha, const value_t* par, int charge = 1, const PID pid = PID::Pion);
   GPUd() const value_t* getParams() const;
   GPUd() value_t getParam(int i) const;
   GPUd() value_t getX() const;
@@ -221,6 +222,7 @@ class TrackParametrization
 
   GPUd() void updateParam(value_t delta, int i);
   GPUd() void updateParams(const params_t& delta);
+  GPUd() void updateParams(const value_t* delta);
 
  private:
   //
@@ -249,6 +251,19 @@ GPUdi() TrackParametrization<value_T>::TrackParametrization(value_t x, value_t a
 //____________________________________________________________
 template <typename value_T>
 GPUdi() void TrackParametrization<value_T>::set(value_t x, value_t alpha, const params_t& par, int charge, const PID pid)
+{
+  mX = x;
+  mAlpha = alpha;
+  mAbsCharge = char(gpu::CAMath::Abs(charge));
+  for (int i = 0; i < kNParams; i++) {
+    mP[i] = par[i];
+  }
+  mPID = pid;
+}
+
+//____________________________________________________________
+template <typename value_T>
+GPUdi() void TrackParametrization<value_T>::set(value_t x, value_t alpha, const value_t* par, int charge, const PID pid)
 {
   mX = x;
   mAlpha = alpha;
@@ -666,6 +681,15 @@ GPUdi() void TrackParametrization<value_T>::updateParam(value_t delta, int i)
 //____________________________________________________________
 template <typename value_T>
 GPUdi() void TrackParametrization<value_T>::updateParams(const params_t& delta)
+{
+  for (int i = kNParams; i--;) {
+    mP[i] += delta[i];
+  }
+}
+
+//____________________________________________________________
+template <typename value_T>
+GPUdi() void TrackParametrization<value_T>::updateParams(const value_t* delta)
 {
   for (int i = kNParams; i--;) {
     mP[i] += delta[i];
