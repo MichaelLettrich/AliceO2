d1bdc0710edea88f3a8831b9cd80f12af362aeb9
Michael Lettrich <michael.lettrich@cern.ch>
2022-06-21 18:23:46 +0200

    [rANS] literal symbol coders in TPC benchmarks
    Use encodersi/decoders that can deal with incompressible symbols in TPC
    benchmarks

--- Utilities/rANS/benchmarks/bench_ransEncoderNew.cxx
+++ Utilities/rANS/benchmarks/bench_ransEncoderNew.cxx
@@ -8,7 +8,8 @@
 #include <benchmark/benchmark.h>
 
 #include "rANS/rans.h"
-#include "rANS/SIMDDecoder.h"
+#include "rANS/LiteralSIMDDecoder.h"
+
 #include "rANS/typetraits.h"
 #include "rANS/renorm.h"
 
@@ -24,7 +25,7 @@ using ransCoder_type = uint64_t;
 using ransStream_type = uint32_t;
 
 template <typename source_T>
-using decoder_type = SIMDDecoder<ransCoder_type, ransStream_type, source_T, NStreams, 1>;
+using decoder_type = LiteralSIMDDecoder<ransCoder_type, ransStream_type, source_T, NStreams, 1>;
 
 inline constexpr size_t MessageSize = 1ull << 22;
 
@@ -88,15 +89,14 @@ void ransCompressionBenchmark(benchmark::State& st)
   DecodeBuffer<source_type> decodeBuffer{inputData.size()};
 
   auto frequencyTable = makeFrequencyTable<containerTag>::fromSamples(gsl::span<const source_type>(inputData));
-  auto renormedFrequencyTable = renormCutoffIncompressible<>(frequencyTable, 20, 10);
+  auto renormedFrequencyTable = renormCutoffIncompressible<>(frequencyTable, 0, 10);
   auto encoder = makeEncoder<coderTag_V>::fromRenormed(renormedFrequencyTable);
-  auto encodeBufferEnd = encodeBuffer.encodeIter;
 
 #ifdef ENABLE_VTUNE_PROFILER
   __itt_resume();
 #endif
   for (auto _ : st) {
-    benchmark::DoNotOptimize(encodeBufferEnd = encoder.process(inputData.data(), inputData.data() + inputData.size(), encodeBuffer.encodeIter));
+    benchmark::DoNotOptimize(encodeBuffer.encodeBufferEnd = encoder.process(inputData.data(), inputData.data() + inputData.size(), encodeBuffer.buffer.data()));
   }
 #ifdef ENABLE_VTUNE_PROFILER
   __itt_pause();
@@ -105,13 +105,83 @@ void ransCompressionBenchmark(benchmark::State& st)
   auto decoderFrequencyTable = makeFrequencyTableFromSamples(inputData.begin(), inputData.end());
   auto decoderRenormed = renormCutoffIncompressible<>(decoderFrequencyTable, renormedFrequencyTable.getRenormingBits(), 10);
   decoder_type<source_type> decoder{decoderRenormed};
-  decoder.process(encodeBufferEnd, decodeBuffer.buffer.data(), inputData.size());
+  decoder.process(encodeBuffer.encodeBufferEnd, decodeBuffer.buffer.data(), inputData.size(), encodeBuffer.literals);
   if (!(decodeBuffer == inputData)) {
     st.SkipWithError("Missmatch between encoded and decoded Message");
   }
 
   st.SetItemsProcessed(static_cast<int64_t>(inputData.size()) * static_cast<int64_t>(st.iterations()));
   st.SetBytesProcessed(static_cast<int64_t>(inputData.size()) * sizeof(source_type) * static_cast<int64_t>(st.iterations()));
+  st.counters["AlphabetRangeBits"] = decoderFrequencyTable.getAlphabetRangeBits();
+  st.counters["nUsedAlphabetSymbols"] = decoderFrequencyTable.getNUsedAlphabetSymbols();
+  st.counters["SymbolTablePrecision"] = decoderRenormed.getRenormingBits();
+  st.counters["Entropy"] = computeEntropy(decoderFrequencyTable);
+  st.counters["ExpectedCodewordLength"] = computeExpectedCodewordLength<source_type>(decoderFrequencyTable, decoderRenormed);
+  st.counters["SourceSize"] = inputData.size() * sizeof(source_type);
+  st.counters["CompressedSize"] = std::distance(encodeBuffer.buffer.data(), encodeBuffer.encodeBufferEnd) * sizeof(ransStream_type);
+  st.counters["Compression"] = st.counters["SourceSize"] / static_cast<double>(st.counters["CompressedSize"]);
+  st.counters["LowerBound"] = inputData.size() * (static_cast<double>(st.counters["Entropy"]) / 8);
+  st.counters["CompressionWRTEntropy"] = st.counters["CompressedSize"] / st.counters["LowerBound"];
+};
+
+template <typename source_T, ContainerTag containerTag_V, CoderTag coderTag_V>
+void ransLiteralCompressionBenchmark(benchmark::State& st)
+{
+  using source_type = source_T;
+  constexpr ContainerTag containerTag = [&]() {
+    if (containerTag_V != ContainerTag::Static) {
+      return containerTag_V;
+    } else {
+      if (sizeof(source_T) < 4) {
+        return ContainerTag::Static;
+      } else {
+        return ContainerTag::Dynamic;
+      }
+    }
+  }();
+
+  const auto& inputData = getMessage<source_type>();
+  EncodeBuffer<source_type> encodeBuffer{inputData.size()};
+  encodeBuffer.literals.resize(inputData.size(), 0);
+  encodeBuffer.literalsEnd = encodeBuffer.literals.data();
+  DecodeBuffer<source_type> decodeBuffer{inputData.size()};
+
+  auto frequencyTable = makeFrequencyTable<containerTag>::fromSamples(gsl::span<const source_type>(inputData));
+  auto renormedFrequencyTable = renormCutoffIncompressible<>(frequencyTable);
+  auto encoder = makeEncoder<coderTag_V>::fromRenormed(renormedFrequencyTable);
+
+#ifdef ENABLE_VTUNE_PROFILER
+  __itt_resume();
+#endif
+  for (auto _ : st) {
+    encodeBuffer.literalsEnd = encodeBuffer.literals.data();
+    benchmark::DoNotOptimize(std::tie(encodeBuffer.encodeBufferEnd, encodeBuffer.literalsEnd) = encoder.process(inputData.data(), inputData.data() + inputData.size(), encodeBuffer.buffer.data(), encodeBuffer.literalsEnd));
+  }
+#ifdef ENABLE_VTUNE_PROFILER
+  __itt_pause();
+#endif
+
+  auto decoderFrequencyTable = makeFrequencyTableFromSamples(inputData.begin(), inputData.end());
+  auto decoderRenormed = renormCutoffIncompressible<>(decoderFrequencyTable, renormedFrequencyTable.getRenormingBits());
+  encodeBuffer.literals.resize(std::distance(encodeBuffer.literals.data(), encodeBuffer.literalsEnd));
+  decoder_type<source_type> decoder{decoderRenormed};
+  decoder.process(encodeBuffer.encodeBufferEnd, decodeBuffer.buffer.data(), inputData.size(), encodeBuffer.literals);
+  if (!(decodeBuffer == inputData)) {
+    st.SkipWithError("Missmatch between encoded and decoded Message");
+  }
+
+  st.SetItemsProcessed(static_cast<int64_t>(inputData.size()) * static_cast<int64_t>(st.iterations()));
+  st.SetBytesProcessed(static_cast<int64_t>(inputData.size()) * sizeof(source_type) * static_cast<int64_t>(st.iterations()));
+  st.counters["AlphabetRangeBits"] = decoderFrequencyTable.getAlphabetRangeBits();
+  st.counters["nUsedAlphabetSymbols"] = decoderFrequencyTable.getNUsedAlphabetSymbols();
+  st.counters["SymbolTablePrecision"] = decoderRenormed.getRenormingBits();
+  st.counters["Entropy"] = computeEntropy(decoderFrequencyTable);
+  st.counters["ExpectedCodewordLength"] = computeExpectedCodewordLength<source_type>(decoderFrequencyTable, decoderRenormed);
+  st.counters["SourceSize"] = inputData.size() * sizeof(source_type);
+  st.counters["CompressedSize"] = std::distance(encodeBuffer.buffer.data(), encodeBuffer.encodeBufferEnd) * sizeof(ransStream_type);
+  st.counters["Compression"] = st.counters["SourceSize"] / static_cast<double>(st.counters["CompressedSize"]);
+  st.counters["LowerBound"] = inputData.size() * (static_cast<double>(st.counters["Entropy"]) / 8);
+  st.counters["CompressionWRTEntropy"] = st.counters["CompressedSize"] / st.counters["LowerBound"];
 };
 
 BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::Compat>);
@@ -140,28 +210,80 @@ BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::Singl
 
 //########################################################################################
 
-BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::SSE>);
-BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::SSE>);
-BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::SSE>);
+BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::SSE>); //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::SSE>); //->Iterations(1000);
 
-BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::SSE>);
-BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::SSE>);
+BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::SSE>); //->Iterations(1000);
 
-BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::SSE>);
-BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::SSE>);
-BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::SSE>);
+BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::SSE>); //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::SSE>); //->Iterations(1000);
 
-//########################################################################################
+// //########################################################################################
 
-BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::AVX2>);
-BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::AVX2>);
-BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::AVX2>);
+BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::AVX2>);  //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::AVX2>); //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::AVX2>); //->Iterations(1000);
 
-BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::AVX2>);
-BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::AVX2>);
+BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::AVX2>);  //->Iterations(1000);
+BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::AVX2>); //->Iterations(1000);
 
 BENCHMARK(ransCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::AVX2>);
 BENCHMARK(ransCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::AVX2>);
 BENCHMARK(ransCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::AVX2>);
+
+//########################################################################################
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::Compat>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::Compat>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::Compat>);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::Compat>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::Compat>);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::Compat>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::Compat>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::Compat>);
+
+//########################################################################################
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::SingleStream>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::SingleStream>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::SingleStream>);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::SingleStream>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::SingleStream>);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::SingleStream>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::SingleStream>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::SingleStream>);
+
+//########################################################################################
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::SSE>); //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::SSE>); //->Iterations(1000);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::SSE>); //->Iterations(1000);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::SSE>);  //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::SSE>); //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::SSE>); //->Iterations(1000);
+
+//########################################################################################
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Dynamic, CoderTag::AVX2>);  //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Dynamic, CoderTag::AVX2>); //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Dynamic, CoderTag::AVX2>); //->Iterations(1000);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Static, CoderTag::AVX2>);  //->Iterations(1000);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Static, CoderTag::AVX2>); //->Iterations(1000);
+
+BENCHMARK(ransLiteralCompressionBenchmark<uint8_t, ContainerTag::Hash, CoderTag::AVX2>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint16_t, ContainerTag::Hash, CoderTag::AVX2>);
+BENCHMARK(ransLiteralCompressionBenchmark<uint32_t, ContainerTag::Hash, CoderTag::AVX2>);
 
 BENCHMARK_MAIN();
