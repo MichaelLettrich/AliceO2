4d7609a92670fc9065c197ba1592fa7967ddf521
Michael Lettrich <michael.lettrich@cern.ch>
2022-07-09 22:32:54 +0200

    f packer

--- Utilities/rANS/include/rANS/Packer.h
+++ Utilities/rANS/include/rANS/Packer.h
@@ -20,13 +20,14 @@
 #include <cstring>
 #include <array>
 
+#include <immintrin.h>
+
 #include <rANS/internal/helper.h>
 
 namespace o2
 {
 namespace rans
 {
-
 inline constexpr std::array<uint64_t, 65> All1BackTill = []() constexpr
 {
   std::array<uint64_t, 65> ret{};
@@ -48,63 +49,87 @@ inline constexpr std::array<uint64_t, 65> All1FrontTill = []() constexpr
 }
 ();
 
-inline void pack(uint64_t*& __restrict__ buffer, size_t& bitOffest, uint64_t data, size_t packingWidth)
+[[nodiscard]] inline uint64_t bitExtract(uint64_t data, uint32_t start, uint32_t length) noexcept
 {
-  constexpr size_t I64Bits = internal::toBits(sizeof(uint64_t));
+#ifdef __BMI__
+  return _bextr_u64(data, start, length);
+#else
+  const uint64_t mask = All1BackTill[start + length] ^ All1BackTill[start];
+  return (data & mask) >> start;
+#endif
+};
 
-  const size_t newBitOffset = bitOffest + packingWidth;
+void pack(uint64_t*& __restrict__ buffer, uint32_t& bitOffset, uint64_t data, uint32_t packingWidth)
+{
+  constexpr uint32_t I64Bits = internal::toBits(sizeof(uint64_t));
+
+  const uint32_t newBitOffset = bitOffset + packingWidth;
   if (newBitOffset <= I64Bits) {
-    *buffer |= data << bitOffest;
-    bitOffest = newBitOffset;
+    *buffer |= data << bitOffset;
+    bitOffset = newBitOffset;
   } else {
-    const size_t leftTail = I64Bits - bitOffest;
-    *buffer |= (data & All1BackTill[leftTail]) << bitOffest;
-    *(++buffer) |= (data >> leftTail);
-    bitOffest = newBitOffset - I64Bits;
+    const uint32_t leftTail = I64Bits - bitOffset;
+    *buffer |= bitExtract(data, 0, leftTail) << bitOffset;
+    *(++buffer) |= data >> leftTail;
+    bitOffset = newBitOffset - I64Bits;
   }
 };
 
 template <typename source_IT>
-inline uint64_t* packStream(const source_IT srcBufferBegin, const source_IT srcBufferEnd, uint64_t* __restrict__ dstBufferBegin, size_t packingWidth)
+inline uint64_t* packStream(const source_IT srcBufferBegin, const source_IT srcBufferEnd, uint64_t* __restrict__ dstBufferBegin, uint32_t packingWidth)
 {
   auto dstIter = dstBufferBegin;
-  size_t pos = 0;
+  uint32_t bitOffset = 0;
   for (auto srcIter = srcBufferBegin; srcIter != srcBufferEnd; ++srcIter) {
-    o2::rans::pack(dstIter, pos, static_cast<uint64_t>(*srcIter), packingWidth);
+    o2::rans::pack(dstIter, bitOffset, static_cast<uint64_t>(*srcIter), packingWidth);
   };
   return ++dstIter;
 };
 
-inline uint64_t unpackNext(const uint64_t*& __restrict__ buffer, size_t& bitOffest, size_t packingWidth)
+uint64_t unpackNext(const uint64_t*& __restrict__ buffer, uint32_t& bitOffset, uint32_t packingWidth)
 {
-  constexpr size_t I64Bits = internal::toBits(sizeof(uint64_t));
-  const size_t bitEnd = bitOffest + packingWidth;
-
+  constexpr uint32_t I64Bits = internal::toBits(sizeof(uint64_t));
+  const uint32_t bitEnd = bitOffset + packingWidth;
   if (bitEnd <= I64Bits) {
-    const uint64_t mask = All1FrontTill[bitOffest] & All1BackTill[bitEnd];
-    uint64_t ret = (*buffer & mask) >> bitOffest;
-    bitOffest = bitEnd;
+    uint64_t ret = bitExtract(*buffer, bitOffset, packingWidth);
+    bitOffset = bitEnd;
     return ret;
   } else {
     // first part
-    uint64_t mask = All1FrontTill[bitOffest];
-    uint64_t value = (*buffer & mask) >> bitOffest;
+    uint64_t value = bitExtract(*buffer, bitOffset, I64Bits - bitOffset);
     //second part
-    bitOffest = bitEnd - I64Bits;
-    mask = All1BackTill[bitOffest];
-    value |= (*(++buffer) & mask) << (packingWidth - bitOffest);
+    bitOffset = bitEnd - I64Bits;
+    value |= bitExtract(*(++buffer), 0, bitOffset) << (packingWidth - bitOffset);
     return value;
   }
 };
 
-inline uint64_t unpackByIndex(const uint64_t* __restrict__ buffer, size_t index, size_t packingWidth)
+uint64_t rUnpackNext(uint64_t*& __restrict__ buffer, uint32_t& bitOffset, uint32_t packingWidth)
 {
-  constexpr size_t I64Bits = internal::toBits(sizeof(uint64_t));
-  constexpr size_t I64Log2 = 6;
+  constexpr uint32_t I64Bits = internal::toBits(sizeof(uint64_t));
+
+  if (packingWidth < bitOffset) {
+    bitOffset -= packingWidth;
+    return bitExtract(*buffer, bitOffset, packingWidth);
+  } else {
+    // first part
+    const uint32_t remainderBits = packingWidth - bitOffset;
+    uint64_t value = bitExtract(*buffer, 0, bitOffset) << remainderBits;
+    //second part
+    bitOffset = 64 - remainderBits;
+    value |= bitExtract(*(--buffer), bitOffset, remainderBits);
+    return value;
+  }
+};
+
+inline uint64_t unpackByIndex(const uint64_t* __restrict__ buffer, size_t index, uint32_t packingWidth)
+{
+  constexpr uint32_t I64Bits = internal::toBits(sizeof(uint64_t));
+  constexpr uint32_t I64Log2 = 6;
 
   const size_t StartBitPos = index * packingWidth;
-  const size_t ArrayIdx = StartBitPos >> I64Log2; // integer div pow2
-  size_t beginElem = StartBitPos & (I64Bits - 1); // integer mod pow2
+  const size_t ArrayIdx = StartBitPos >> I64Log2;   // integer div pow2
+  uint32_t beginElem = StartBitPos & (I64Bits - 1); // integer mod pow2
 
   const uint64_t* iter = buffer + ArrayIdx;
 
@@ -112,14 +137,53 @@ inline uint64_t unpackByIndex(const uint64_t* __restrict__ buffer, size_t index,
 };
 
 template <typename source_T>
-inline void unpackStream(const uint64_t* __restrict__ packingBufferBegin, source_T* __restrict__ destBufferBegin, size_t nElements, size_t packingWidth)
+inline void unpackStream(const uint64_t* __restrict__ packingBufferBegin, source_T* __restrict__ destBufferBegin, size_t nElements, uint32_t packingWidth)
 {
   auto iter = packingBufferBegin;
-  size_t bitOffset = 0;
+  uint32_t bitOffset = 0;
 
   for (size_t i = 0; i < nElements; ++i) {
     *(destBufferBegin++) = static_cast<source_T>(unpackNext(iter, bitOffset, packingWidth));
   }
+};
+
+void deltaEncode(uint64_t*& __restrict__ buffer, uint32_t& bitOffset, uint32_t data)
+{
+  const uint32_t highestPow2 = internal::log2UIntNZ(data);
+  const uint32_t nLeadingZeros = internal::log2UIntNZ(highestPow2 + 1);
+  uint64_t value = highestPow2 + 1;
+  value = value << highestPow2 | bitExtract(data, 0, highestPow2);
+  uint32_t packingOffset = nLeadingZeros + nLeadingZeros + 1 + highestPow2;
+
+  pack(buffer, bitOffset, value, packingOffset);
+};
+
+uint32_t deltaDecode(uint64_t*& __restrict__ buffer, uint32_t& bitOffset)
+{
+  constexpr uint32_t I64Bits = internal::toBits(sizeof(uint64_t));
+  constexpr uint32_t maxSymbolBits = 42; // a 32 bit integer can at max use 42 Bits with delta coding;
+
+  // unpack maxSymbolBits bits of data
+  uint64_t* unpackIter = buffer;
+  uint32_t unpackPos = bitOffset;
+  uint64_t rawData = rUnpackNext(unpackIter, unpackPos, maxSymbolBits);
+
+  // do delta decoding
+  rawData <<= I64Bits - maxSymbolBits;
+  const uint32_t nLeadingZeros = __builtin_clzl(rawData);
+  uint32_t packingOffset = 2 * nLeadingZeros + 1;
+  const uint32_t highestPow2 = bitExtract(rawData, I64Bits - packingOffset, nLeadingZeros + 1) - 1;
+  packingOffset += highestPow2;
+  uint32_t value = (1 << highestPow2) + bitExtract(rawData, I64Bits - packingOffset, highestPow2);
+
+  // set out parameters
+  if (packingOffset > bitOffset) {
+    bitOffset = I64Bits + bitOffset - packingOffset;
+    --buffer;
+  } else {
+    bitOffset -= packingOffset;
+  }
+  return value;
 };
 
 } // namespace rans
