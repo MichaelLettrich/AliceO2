b3b00f9d53ba1feaa5c5e410f914ce1e299215e3
Michael Lettrich <michael.lettrich@cern.ch>
2022-07-11 11:52:56 +0200

    unittests packing/eliasdelta

--- Utilities/rANS/test/test_ransPacker.cxx
+++ Utilities/rANS/test/test_ransPacker.cxx
@@ -24,10 +24,11 @@
 
 #include <boost/test/unit_test.hpp>
 #include <boost/mpl/vector.hpp>
+#include <fmt/core.h>
 
 #include "rANS/Packer.h"
 
-using source_type = uint32_t;
+using source_type = uint64_t;
 
 std::vector<source_type> generate(source_type minNumber, source_type maxNumber)
 {
@@ -40,46 +41,159 @@ std::vector<source_type> generate(source_type minNumber, source_type maxNumber)
   return ret;
 };
 
-BOOST_AUTO_TEST_CASE(test_packUnpack)
+BOOST_AUTO_TEST_CASE(test_pack)
 {
-  constexpr size_t packWidth = 17;
 
-  auto source = generate(0, (1u << packWidth) - 1);
-  std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
-  std::vector<uint8_t> packingBuffer2(source.size() * sizeof(source_type), 0);
+  for (size_t packingWidth = 1; packingWidth <= 63; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Packing", packingWidth));
 
-  // use element wise API
-  uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
-  size_t pos = 0;
-  for (auto i : source) {
-    o2::rans::pack(iter, pos, i, packWidth);
-  };
+    auto source = generate(0, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
 
-  // use buffer API
-  auto packingBuffer2End = reinterpret_cast<uint8_t*>(o2::rans::packStream(source.begin(),
-                                                                           source.end(),
-                                                                           reinterpret_cast<uint64_t*>(packingBuffer2.data()),
-                                                                           packWidth));
+    uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    uint32_t bitOffset = 0;
+    for (auto i : source) {
+      o2::rans::pack(iter, bitOffset, i, packingWidth);
+    };
 
-  // check if results are equal;
-  BOOST_CHECK_EQUAL_COLLECTIONS(packingBuffer.data(), reinterpret_cast<uint8_t*>(++iter), packingBuffer2.data(), packingBuffer2End);
+    std::vector<uint8_t> packingBuffer2(source.size() * sizeof(source_type), 0);
 
-  //unpack using indexed access
-  std::vector<source_type> unpackBuffer;
-  iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
-  for (size_t i = 0; i < source.size(); ++i) {
-    unpackBuffer.push_back(o2::rans::unpackByIndex(iter, i, packWidth));
-  };
+    // use buffer API
+    auto packingBuffer2End = reinterpret_cast<uint8_t*>(o2::rans::packStream(source.begin(),
+                                                                             source.end(),
+                                                                             reinterpret_cast<uint64_t*>(packingBuffer2.data()),
+                                                                             packingWidth));
 
-  std::vector<source_type> unpackBuffer2(source.size(), 0);
+    // check if results are equal;
+    BOOST_CHECK_EQUAL_COLLECTIONS(packingBuffer.data(), reinterpret_cast<uint8_t*>(++iter), packingBuffer2.data(), packingBuffer2End);
+  }
+};
 
-  // unpack full buffer
-  o2::rans::unpackStream(reinterpret_cast<uint64_t*>(packingBuffer.data()),
-                         unpackBuffer2.data(),
-                         source.size(),
-                         packWidth);
+BOOST_AUTO_TEST_CASE(test_packUnpackNext)
+{
 
-  // compare if both yield the correct result
-  BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.begin(), unpackBuffer.end());
-  BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer2.begin(), unpackBuffer2.end());
+  for (size_t packingWidth = 1; packingWidth <= 63; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Packing", packingWidth));
+    auto source = generate(0, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
+
+    uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    uint32_t bitOffset = 0;
+    for (auto i : source) {
+      o2::rans::pack(iter, bitOffset, i, packingWidth);
+    };
+
+    //unpack next
+    std::vector<source_type> unpackBuffer;
+    const uint64_t* unpackIter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    bitOffset = 0;
+    for (size_t i = 0; i < source.size(); ++i) {
+      unpackBuffer.push_back(o2::rans::unpackNext(unpackIter, bitOffset, packingWidth));
+    };
+
+    // compare if both yield the correct result
+    BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.begin(), unpackBuffer.end());
+  }
+};
+
+BOOST_AUTO_TEST_CASE(test_packRUnpackNext)
+{
+
+  for (size_t packingWidth = 1; packingWidth <= 63; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Packing", packingWidth));
+    auto source = generate(0, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
+
+    uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    uint32_t bitOffset = 0;
+    for (auto i : source) {
+      o2::rans::pack(iter, bitOffset, i, packingWidth);
+    };
+
+    //unpack next
+    std::vector<source_type> unpackBuffer;
+    for (size_t i = 0; i < source.size(); ++i) {
+      unpackBuffer.push_back(o2::rans::rUnpackNext(iter, bitOffset, packingWidth));
+    };
+
+    // compare if both yield the correct result
+    BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.rbegin(), unpackBuffer.rend());
+  }
+};
+
+BOOST_AUTO_TEST_CASE(test_packUnpackByIdx)
+{
+
+  for (size_t packingWidth = 1; packingWidth <= 63; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Packing", packingWidth));
+    auto source = generate(0, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
+
+    uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    uint32_t bitOffset = 0;
+    for (auto i : source) {
+      o2::rans::pack(iter, bitOffset, i, packingWidth);
+    };
+
+    //unpack using indexed access
+    std::vector<source_type> unpackBuffer;
+    iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    for (size_t i = 0; i < source.size(); ++i) {
+      unpackBuffer.push_back(o2::rans::unpackByIndex(iter, i, packingWidth));
+    };
+
+    // compare if both yield the correct result
+    BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.begin(), unpackBuffer.end());
+  }
+};
+
+BOOST_AUTO_TEST_CASE(test_packUnpackStream)
+{
+
+  for (size_t packingWidth = 1; packingWidth <= 63; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Packing", packingWidth));
+    auto source = generate(0, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
+
+    // use buffer API
+    auto packingBufferEnd = reinterpret_cast<uint8_t*>(o2::rans::packStream(source.begin(),
+                                                                            source.end(),
+                                                                            reinterpret_cast<uint64_t*>(packingBuffer.data()),
+                                                                            packingWidth));
+
+    std::vector<source_type> unpackBuffer(source.size(), 0);
+
+    // unpack full buffer
+    o2::rans::unpackStream(reinterpret_cast<uint64_t*>(packingBuffer.data()),
+                           unpackBuffer.data(),
+                           source.size(),
+                           packingWidth);
+
+    // compare if both yield the correct result
+    BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.begin(), unpackBuffer.end());
+  }
+};
+
+BOOST_AUTO_TEST_CASE(test_packRUnpackEliasDelta)
+{
+
+  for (size_t packingWidth = 1; packingWidth <= 32; ++packingWidth) {
+    BOOST_TEST_MESSAGE(fmt::format("Checking {} Bit Elias Delta Coder", packingWidth));
+    auto source = generate(1ull, (1ull << packingWidth) - 1);
+    std::vector<uint8_t> packingBuffer(source.size() * sizeof(source_type), 0);
+
+    uint64_t* iter = reinterpret_cast<uint64_t*>(packingBuffer.data());
+    uint32_t bitOffset = 0;
+    for (auto i : source) {
+      o2::rans::eliasDeltaEncode(iter, bitOffset, i);
+    };
+
+    std::vector<uint32_t> unpackBuffer;
+    for (size_t i = 0; i < source.size(); ++i) {
+      unpackBuffer.push_back(o2::rans::eliasDeltaDecode(iter, bitOffset));
+    };
+
+    // compare if both yield the correct result
+    BOOST_CHECK_EQUAL_COLLECTIONS(source.begin(), source.end(), unpackBuffer.rbegin(), unpackBuffer.rend());
+  }
 };
