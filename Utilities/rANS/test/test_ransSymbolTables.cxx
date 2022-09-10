c545838eb7c1813ee918e8fdbfc5fb681a980ab5
Michael Lettrich <michael.lettrich@cern.ch>
2022-09-10 15:40:28 +0200

    [rANS] Reorganize librans
    New directory structure, renamed classes and files to
    match the purpmatch the purpose and intended usage of files.

--- Utilities/rANS/test/test_ransSymbolTables.cxx
+++ Utilities/rANS/test/test_ransSymbolTables.cxx
@@ -24,14 +24,12 @@
 #include <numeric>
 #include <iterator>
 
-#include "rANS/FrequencyTable.h"
-
-#include "rANS/renorm.h"
-
-#include "rANS/internal/Symbol.h"
-#include "rANS/SymbolTable.h"
+#include "rANS/histogram.h"
+#include "rANS/internal/containers/SymbolTable.h"
+#include "rANS/internal/containers/Symbol.h"
 
 using namespace o2::rans;
+using namespace o2::rans::internal;
 
 template <typename T>
 size_t getNUniqueSymbols(const T& container)
@@ -39,20 +37,20 @@ size_t getNUniqueSymbols(const T& container)
   return std::count_if(container.begin(), container.end(), [](uint32_t value) { return value != 0; });
 };
 
-using frequencyTable_t = boost::mpl::vector<FrequencyTable<uint8_t>,
-                                            FrequencyTable<int8_t>,
-                                            FrequencyTable<uint16_t>,
-                                            FrequencyTable<int16_t>,
-                                            FrequencyTable<uint32_t>,
-                                            FrequencyTable<int32_t>>;
+using histogram_t = boost::mpl::vector<Histogram<uint8_t>,
+                                       Histogram<int8_t>,
+                                       Histogram<uint16_t>,
+                                       Histogram<int16_t>,
+                                       Histogram<uint32_t>,
+                                       Histogram<int32_t>>;
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(test_empty, frequencyTable_T, frequencyTable_t)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_empty, histogram_T, histogram_t)
 {
   using namespace o2::rans;
 
-  using source_type = typename frequencyTable_T::source_type;
+  using source_type = typename histogram_T::source_type;
   std::vector<source_type> samples{};
-  frequencyTable_T f{};
+  histogram_T f{};
   f.addSamples(gsl::make_span(samples));
 
   const auto symbolTable = SymbolTable<source_type, internal::Symbol>(renormCutoffIncompressible(f));
@@ -80,18 +78,17 @@ BOOST_AUTO_TEST_CASE_TEMPLATE(test_empty, frequencyTable_T, frequencyTable_t)
   }
 }
 
-BOOST_AUTO_TEST_CASE_TEMPLATE(test_symbolTable, frequencyTable_T, frequencyTable_t)
+BOOST_AUTO_TEST_CASE_TEMPLATE(test_symbolTable, histogram_T, histogram_t)
 {
-  using source_type = typename frequencyTable_T::source_type;
+  using source_type = typename histogram_T::source_type;
 
-  histogram_t frequencies{1, 1, 2, 2, 2, 2, 6, 8, 4, 10, 8, 14, 10, 19, 26, 30, 31, 35, 41, 45, 51, 44, 47, 39, 58, 52, 42, 53, 50, 34, 50, 30, 32, 24, 30, 20, 17, 12, 16, 6, 8, 5, 6, 4, 4, 2, 2, 2, 1};
-  frequencyTable_T frequencyTable{frequencies.begin(), frequencies.end(), static_cast<uint8_t>(0)};
+  std::vector<uint32_t> frequencies{1, 1, 2, 2, 2, 2, 6, 8, 4, 10, 8, 14, 10, 19, 26, 30, 31, 35, 41, 45, 51, 44, 47, 39, 58, 52, 42, 53, 50, 34, 50, 30, 32, 24, 30, 20, 17, 12, 16, 6, 8, 5, 6, 4, 4, 2, 2, 2, 1};
+  histogram_T histogram{frequencies.begin(), frequencies.end(), static_cast<uint8_t>(0)};
   const size_t scaleBits = 8;
-  auto renormedFrequencyTable = renormCutoffIncompressible(std::move(frequencyTable), scaleBits, 1);
-  const auto symbolTable = SymbolTable<source_type, internal::Symbol>(renormedFrequencyTable);
+  const auto symbolTable = SymbolTable<source_type, internal::Symbol>(renormCutoffIncompressible(std::move(histogram), scaleBits, 1));
 
-  const histogram_t rescaledFrequencies{1, 2, 1, 3, 2, 3, 3, 5, 6, 7, 8, 9, 10, 11, 13, 11, 12, 10, 14, 13, 10, 13, 12, 8, 12, 7, 8, 6, 7, 5, 4, 3, 4, 2, 2, 1, 2, 1, 1};
-  histogram_t cumulativeFrequencies;
+  const std::vector<uint32_t> rescaledFrequencies{1, 2, 1, 3, 2, 3, 3, 5, 6, 7, 8, 9, 10, 11, 13, 11, 12, 10, 14, 13, 10, 13, 12, 8, 12, 7, 8, 6, 7, 5, 4, 3, 4, 2, 2, 1, 2, 1, 1};
+  std::vector<uint32_t> cumulativeFrequencies;
   std::exclusive_scan(rescaledFrequencies.begin(), rescaledFrequencies.end(), std::back_inserter(cumulativeFrequencies), 0);
 
   BOOST_CHECK_EQUAL(symbolTable.getPrecision(), scaleBits);
