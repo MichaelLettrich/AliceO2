bdf72f677866e8e3a2e12cf2aac4cf8bb0e7734e
Michael Lettrich <michael.lettrich@cern.ch>
2022-10-29 13:02:15 +0200

    [rANS] Choose rANS implementation at compile time
    * use pre-processor includes to restrict choice of available encoding
    algorithms:
      * rANS compat for all 64 bit systems
      * rANS single-stream for all systems that define 128 Bit types
      * rANS SSE for all x86_64 systems with at least SSE 4.1
      * rANS AVX2 for all x86_64 systems with at-least AVX2 and FMA
    
    Set the fastest possible implementation as default template parameters in factory classes, but
    allow manual override.

--- Utilities/rANS/include/rANS/internal/common/simdops.h
+++ Utilities/rANS/include/rANS/internal/common/simdops.h
@@ -15,6 +15,10 @@
 #ifndef RANS_INTERNAL_COMMON_SIMD_H_
 #define RANS_INTERNAL_COMMON_SIMD_H_
 
+#include "rANS/internal/common/defines.h"
+
+#ifdef RANS_SIMD
+
 #include <immintrin.h>
 
 #include <gsl/span>
@@ -59,7 +63,7 @@ inline __m128d load(const pd_t<SIMDWidth::SSE>& v) noexcept
   return _mm_load_pd(v.data());
 };
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 
 template <typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
 inline __m256i load(const AlignedArray<T, SIMDWidth::AVX>& v) noexcept
@@ -94,7 +98,7 @@ inline __m256d load(gsl::span<const double_t, getElementCount<double_t>(SIMDWidt
   return _mm256_load_pd(v.data());
 };
 
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
 
 template <typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
 inline AlignedArray<T, SIMDWidth::SSE> store(__m128i inVec) noexcept
@@ -122,7 +126,7 @@ inline void store(__m128d inVec, gsl::span<double_t, getElementCount<double>(SIM
   _mm_store_pd(v.data(), inVec);
 };
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 
 template <typename T, std::enable_if_t<std::is_integral_v<T>, bool> = true>
 inline AlignedArray<T, SIMDWidth::AVX> store(__m256i inVec) noexcept
@@ -150,7 +154,7 @@ inline void store(__m256d inVec, gsl::span<double_t, getElementCount<double>(SIM
   _mm256_store_pd(v.data(), inVec);
 };
 
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
 
 template <SIMDWidth width_V>
 inline auto setAll(uint64_t value) noexcept
@@ -211,9 +215,9 @@ inline auto int32ToDouble(__m128i in) noexcept
   if constexpr (width_V == SIMDWidth::SSE) {
     return _mm_cvtepi32_pd(in);
   } else if constexpr (width_V == SIMDWidth::AVX) {
-#ifdef __AVX2__
+#ifdef RANS_AVX2
     return _mm256_cvtepi32_pd(in);
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
   }
 };
 
@@ -239,7 +243,7 @@ inline __m128d uint64ToDouble(__m128i in) noexcept
   return out;
 };
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 //
 // uint64 -> double
 // Only works for inputs in the range: [0, 2^52)
@@ -256,7 +260,7 @@ inline __m256d uint64ToDouble(__m256i in) noexcept
   __m256d out = _mm256_sub_pd(_mm256_castsi256_pd(in), _mm256_set1_pd(AlignMantissaMagic));
   return out;
 };
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
 
 inline __m128i doubleToUint64(__m128d in) noexcept
 {
@@ -272,7 +276,7 @@ inline __m128i doubleToUint64(__m128d in) noexcept
   return out;
 }
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 
 inline __m256i doubleToUint64(__m256d in) noexcept
 {
@@ -289,7 +293,7 @@ inline __m256i doubleToUint64(__m256d in) noexcept
   return out;
 }
 
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
 
 template <SIMDWidth>
 struct DivMod;
@@ -305,11 +309,16 @@ inline DivMod<SIMDWidth::SSE>
   divMod(__m128d numerator, __m128d denominator) noexcept
 {
   __m128d div = _mm_floor_pd(_mm_div_pd(numerator, denominator));
+#ifdef RANS_FMA
   __m128d mod = _mm_fnmadd_pd(div, denominator, numerator);
+#else /* !defined(RANS_FMA) */
+  __m128d mod = _mm_mul_pd(div, denominator);
+  mod = _mm_sub_pd(numerator, mod);
+#endif
   return {div, mod};
 }
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 
 template <>
 struct DivMod<SIMDWidth::AVX> {
@@ -324,7 +333,7 @@ inline DivMod<SIMDWidth::AVX> divMod(__m256d numerator, __m256d denominator) noe
   __m256d mod = _mm256_fnmadd_pd(div, denominator, numerator);
   return {div, mod};
 }
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
 
 inline __m128i cmpgeq_epi64(__m128i a, __m128i b) noexcept
 {
@@ -333,15 +342,127 @@ inline __m128i cmpgeq_epi64(__m128i a, __m128i b) noexcept
   return _mm_or_si128(cmpGreater, cmpEqual);
 };
 
-#ifdef __AVX2__
+#ifdef RANS_AVX2
 inline __m256i cmpgeq_epi64(__m256i a, __m256i b) noexcept
 {
   __m256i cmpGreater = _mm256_cmpgt_epi64(a, b);
   __m256i cmpEqual = _mm256_cmpeq_epi64(a, b);
   return _mm256_or_si256(cmpGreater, cmpEqual);
 };
-#endif /* __AVX2__ */
+#endif /* RANS_AVX2 */
+
+inline std::pair<uint32_t, uint32_t> minmax(const uint32_t* begin, const uint32_t* end)
+{
+  constexpr size_t ElemsPerLane = 4;
+  constexpr size_t nUnroll = 2 * ElemsPerLane;
+  auto iter = begin;
+
+  uint32_t min = *iter;
+  uint32_t max = *iter;
+
+  if (end - nUnroll > begin) {
+
+    __m128i minVec[2];
+    __m128i maxVec[2];
+    __m128i tmpVec[2];
+
+    minVec[0] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter));
+    minVec[1] = minVec[0];
+    maxVec[0] = minVec[0];
+    maxVec[1] = minVec[0];
+
+    for (; iter < end - nUnroll; iter += nUnroll) {
+      tmpVec[0] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter));
+      minVec[0] = _mm_min_epu32(minVec[0], tmpVec[0]);
+      maxVec[0] = _mm_max_epu32(maxVec[0], tmpVec[0]);
+
+      tmpVec[1] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter) + 1);
+      minVec[1] = _mm_min_epu32(minVec[1], tmpVec[1]);
+      maxVec[1] = _mm_max_epu32(maxVec[1], tmpVec[1]);
+
+      __builtin_prefetch(iter + 512, 0);
+    }
+
+    minVec[0] = _mm_min_epu32(minVec[0], minVec[1]);
+    maxVec[0] = _mm_max_epu32(maxVec[0], maxVec[1]);
+
+    uint32_t tmpMin[ElemsPerLane];
+    uint32_t tmpMax[ElemsPerLane];
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(tmpMin), minVec[0]);
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(tmpMax), maxVec[0]);
+
+    for (size_t i = 0; i < ElemsPerLane; ++i) {
+      min = std::min(tmpMin[i], min);
+      max = std::max(tmpMax[i], max);
+    }
+  }
+
+  while (iter != end) {
+    min = std::min(*iter, min);
+    max = std::max(*iter, max);
+    ++iter;
+  }
+
+  return {min, max};
+};
+
+inline std::pair<int32_t, int32_t> minmax(const int32_t* begin, const int32_t* end)
+{
+  constexpr size_t ElemsPerLane = 4;
+  constexpr size_t nUnroll = 2 * ElemsPerLane;
+  auto iter = begin;
+
+  int32_t min = *iter;
+  int32_t max = *iter;
+
+  if (end - nUnroll > begin) {
+
+    __m128i minVec[2];
+    __m128i maxVec[2];
+    __m128i tmpVec[2];
+
+    minVec[0] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter));
+    minVec[1] = minVec[0];
+    maxVec[0] = minVec[0];
+    maxVec[1] = minVec[0];
+
+    for (; iter < end - nUnroll; iter += nUnroll) {
+      tmpVec[0] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter));
+      minVec[0] = _mm_min_epi32(minVec[0], tmpVec[0]);
+      maxVec[0] = _mm_max_epi32(maxVec[0], tmpVec[0]);
+
+      tmpVec[1] = _mm_loadu_si128(reinterpret_cast<const __m128i_u*>(iter) + 1);
+      minVec[1] = _mm_min_epi32(minVec[1], tmpVec[1]);
+      maxVec[1] = _mm_max_epi32(maxVec[1], tmpVec[1]);
+
+      __builtin_prefetch(iter + 512, 0);
+    }
+
+    minVec[0] = _mm_min_epi32(minVec[0], minVec[1]);
+    maxVec[0] = _mm_max_epi32(maxVec[0], maxVec[1]);
+
+    int32_t tmpMin[ElemsPerLane];
+    int32_t tmpMax[ElemsPerLane];
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(tmpMin), minVec[0]);
+    _mm_storeu_si128(reinterpret_cast<__m128i*>(tmpMax), maxVec[0]);
+
+    for (size_t i = 0; i < ElemsPerLane; ++i) {
+      min = std::min(tmpMin[i], min);
+      max = std::max(tmpMax[i], max);
+    }
+  }
+
+  while (iter != end) {
+    min = std::min(*iter, min);
+    max = std::max(*iter, max);
+    ++iter;
+  }
+
+  return {min, max};
+};
 
 } // namespace o2::rans::internal::simd
+
+#endif /* RANS_SIMD */
 
 #endif /* RANS_INTERNAL_COMMON_SIMD_H_ */
