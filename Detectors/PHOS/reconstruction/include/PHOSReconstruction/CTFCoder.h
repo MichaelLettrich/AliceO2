9846ef72601a5f5535cdb1458d9843f3cc03a567
Michael Lettrich <michael.lettrich@cern.ch>
2023-04-22 17:48:56 +0200

    [ctf] Allow data-driven encoding/packing decision

--- Detectors/PHOS/reconstruction/include/PHOSReconstruction/CTFCoder.h
+++ Detectors/PHOS/reconstruction/include/PHOSReconstruction/CTFCoder.h
@@ -84,13 +84,13 @@ o2::ctf::CTFIOSize CTFCoder::encode_impl(VEC& buff, const gsl::span<const Trigge
   using MD = o2::ctf::Metadata::OptStore;
   // what to do which each field: see o2::ctd::Metadata explanation
   constexpr MD optField[CTF::getNBlocks()] = {
-    MD::EENCODE, // BLC_bcIncTrig
-    MD::EENCODE, // BLC_orbitIncTrig
-    MD::EENCODE, // BLC_entriesTrig
-    MD::EENCODE, // BLC_packedID
-    MD::EENCODE, // BLC_time
-    MD::EENCODE, // BLC_energy
-    MD::EENCODE  // BLC_status
+    MD::EENCODE_OR_PACK, // BLC_bcIncTrig
+    MD::EENCODE_OR_PACK, // BLC_orbitIncTrig
+    MD::EENCODE_OR_PACK, // BLC_entriesTrig
+    MD::EENCODE_OR_PACK, // BLC_packedID
+    MD::EENCODE_OR_PACK, // BLC_time
+    MD::EENCODE_OR_PACK, // BLC_energy
+    MD::EENCODE_OR_PACK  // BLC_status
   };
 
   CTFHelper helper(trigData, cellData);
